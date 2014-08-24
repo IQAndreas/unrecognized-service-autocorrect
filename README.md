4ca731334c0418346095a85a763d7ddde82c0024
Andreas Renberg (IQAndreas) <contact@iqandreas.com>
2014-08-24 13:50:25 -0500

    Add an alternative version which automatically corrects flipped arguments
    The second version can be installed instead by passing `--force` to the
    `install.sh` script.

--- README.md
+++ README.md
@@ -13,11 +13,21 @@ restart: unrecognized service
 Did you mean 'service apache2 restart'?
 ```
 
+There is a second mode in which the script will automatically try to run the command you meant to run, not just give a warning:
+
+```
+$ sudo service restart apache2
+restart: unrecognized service
+You probably meant 'service apache2 restart'
+ * Restarting web server apache2
+...
+```
+
 ### Installation
 
-To add this capability to your system, simply run `install.sh` (which will likely require elevated privelages). 
+To add this capability to your system, simply run `install.sh` (which will likely require elevated privelages). If you want to install the version which only warns you about the flipped arguments, use the `--warn` flag or no arguments at all. If you want the version which automatically tries to run the command you meant to run, use the `--force` flag.
 
-It will add the shell script `/usr/bin/unrecognized-service-autocorrect`, and soft links that point there from the following locations:
+The installation will add the shell script `/usr/bin/unrecognized-service-autocorrect`, and soft links that point there from the following locations:
 
 ```
  * /etc/init.d/start
--- install.sh
+++ install.sh
@@ -2,17 +2,29 @@
 # Installs a single 'unrecognized-service-autocorrect' script to '/usr/bin/'
 # and creates links in '/etc/init.d/' from all the common service commands to aforementioned script
 
+if [ "$#" -lt 1 ] || [ "$1" == '--warn' ]; then
+	source_bin='unrecognized-service-autocorrect-warn'
+elif [ "$1" == '--force' ]; then
+	source_bin='unrecognized-service-autocorrect-force'
+else
+	echo "Unknown argument '$1'."
+	echo "Valid parameters are '--warn' or '--force'".
+	exit 1;
+fi
+
+target_bin='/usr/bin/unrecognized-service-autocorrect'
+
 # This may need to be run as root in order to work
-cp "unrecognized-service-autocorrect" "/usr/bin/unrecognized-service-autocorrect" || exit 1;
+cp "$source_bin" "$target_bin" || exit 1;
 
 # Just in case permissions weren't preserved when the script was downloaded
-chmod +x "/usr/bin/unrecognized-service-autocorrect" || exit 1;
+chmod +x "$target_bin" || exit 1;
 
 commands=( start stop reload force-reload restart status )
 for command in "${commands[@]}"; do
 	# Will not overwrite the command if it already exists.
 	# Who knows, maybe some day someone will create a service named 'restart'?
 	if [ ! -e "$command" ]; then
-		ln -s "/usr/bin/unrecognized-service-autocorrect" "/etc/init.d/$command"
+		ln -s "$target_bin" "/etc/init.d/$command"
 	fi
 done
